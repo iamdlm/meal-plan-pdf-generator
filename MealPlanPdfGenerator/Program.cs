734ec4d8a776f0840fb322bdaf3c8694f284389a
Diogo Mesquita <diogoluismesquita@outlook.com>
2025-05-04 16:05:54 +0100

    Initial commit

---
+++ MealPlanPdfGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MealPlanPdfGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
